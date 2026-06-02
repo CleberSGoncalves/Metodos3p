3f6a5294494af3341e764f7e5f45b26ecb8dab85
Cleber <binhole@gmail.com>
2026-06-02 09:05:47 -0300

    chore: renomeia decisao.js e conteudos.js para forcar limpeza agressiva de cache nos celulares

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-reformas-3p-v2.0.4';
+const CACHE_NAME = 'cache-reformas-3p-v3.0.0';
 
 // Install event - force immediate activation
 self.addEventListener('install', event => {
