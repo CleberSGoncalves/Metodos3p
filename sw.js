31a72608e64ee835d233671a0fc39cf5d0879166
Cleber <binhole@gmail.com>
2026-06-03 06:15:02 -0300

    fix: remove display flex from ID selectors to allow display none tab hide to work when switching tabs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cache-reformas-3p-v3.0.11';
+const CACHE_NAME = 'cache-reformas-3p-v3.0.12';
 
 // Install event - force immediate activation
 self.addEventListener('install', event => {
